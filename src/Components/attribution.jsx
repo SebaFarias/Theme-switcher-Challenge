60b849c9c3581c62a33d7ce71d354a9192f6ecf3
S F <sebastian.fariasb@usach.cl>
2020-09-29 23:06:06 -0300

    initial config

---
+++ src/Components/attribution.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const attribution = () =>{
+    return(
+    <div className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a href="#">Your Name Here</a>.
+    </div>
+    )
+} 
+
+export default attribution
