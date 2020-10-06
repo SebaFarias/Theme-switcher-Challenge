52388e25744f0a9452d5c9b342b755a4cd1ba10c
S F <sebastian.fariasb@usach.cl>
2020-10-06 19:13:38 -0300

    favIcon fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import favIcon from '../images/favicon-32x32.png'
 import App from './App';
+
+const icon = document.createElement("link")
+icon.setAttribute("type","image/png")
+icon.setAttribute("rel","icon")
+icon.setAttribute("sizes","32x32")
+icon.href = favIcon
+document.head.appendChild(icon)
 
 ReactDOM.render(<App/>,document.getElementById('root'));
