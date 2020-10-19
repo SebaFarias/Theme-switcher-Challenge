6bc5a52d974349b53315c09022251d34e381398c
S F <sebastian.fariasb@usach.cl>
2020-10-19 01:41:42 -0300

    Revert "Favicon and fonts removed from index.js"
    This reverts commit f2f2390cd6ea6c28c5b7c6ac27c1f4e4b1cb61dd.

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,26 @@ import React, {useState , useCallback} from 'react';
 import ThemeContext , { DARK_THEME , LIGHT_THEME , DarkMode } from './theme';
 import Layout from './Components/Layout'
 import './styles/style.css';
-import './styles/fonts.css';
 
 
 const App = () => {
-	
-	const [ dark , setDark ] = useState(DarkMode.getTheme)
-	const theme = dark ? DARK_THEME : LIGHT_THEME
+    
+    const [ dark , setDark ] = useState(DarkMode.getTheme)
+    const theme = dark ? DARK_THEME : LIGHT_THEME
 
-	const toggleDarkMode = useCallback(() => {
-		setDark( prevState => {
-			const newState = !prevState;
-			DarkMode.setTheme(newState);
-			return newState;
-		});
-	},[dark]);
+    const toggleDarkMode = useCallback(() => {
+        setDark( prevState => {
+            const newState = !prevState;
+            DarkMode.setTheme(newState);
+            return newState;
+        });
+    },[dark]);
 
-	return(
-		<ThemeContext.Provider value={theme}>       
-			<Layout state={dark} toggler={toggleDarkMode}/>            
-		</ThemeContext.Provider>
-	)
+    return(
+        <ThemeContext.Provider value={theme}>       
+            <Layout state={dark} toggler={toggleDarkMode}/>            
+        </ThemeContext.Provider>
+    )
 }
 
 export default App
