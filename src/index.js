5d253136e379ed88c93fad644ed9777d0203b13f
S F <sebastian.fariasb@usach.cl>
2020-10-19 01:43:46 -0300

    Revert "Favicon and fonts removed from index.js"
    This reverts commit a1aac6b813118ac7cbfbc74b12048297266b03ca.

--- src/index.js
+++ src/index.js
@@ -2,4 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+const fonts = document.createElement('link')
+fonts.href = 'https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap' 
+fonts.setAttribute('rel','stylesheet')
+document.head.appendChild(fonts)
+
 ReactDOM.render(<App/>,document.getElementById('root'));
